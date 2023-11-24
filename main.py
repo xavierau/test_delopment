137ffdf00feb7ad41d600e6ce0ac9e848face2cc
Xavier Au <xavierau@TJ3VFW49GN.local>
2023-11-24 14:08:10 +0800

    update

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.post("/")
+async def post_root():
+    return {"message": "Hello World",
+            "method": "POST"}
+
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
